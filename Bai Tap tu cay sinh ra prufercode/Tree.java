2c0be23ccf7ae4f268b7605ed89ae02dada900c0
phamnam1805 <57164967+phamnam1805@users.noreply.github.com>
2019-11-07 20:57:08 +0700

    Update Tree.java

--- Bai Tap tu cay sinh ra prufercode/Tree.java
+++ Bai Tap tu cay sinh ra prufercode/Tree.java
@@ -1,27 +1,28 @@
-
+// Bai tap sinh prufercode tu cay
 import java.util.Scanner;
 
 public class Tree {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n;
-        int[][] tree;
-        int[] degree;
-        int[] prufer;
+        int[][] tree; // mang luu cac canh cua cay
+        int[] degree; // mang luu bac cua cac dinh
+        int[] prufer; // mang luu prufercode
         System.out.println("Nhập số cạnh của cây: ");
         n =  scan.nextInt();
-        tree = new int[n][2];
-        prufer = new int[n-1];
-        degree = new int[n+1];
+        tree = new int[n][2]; // khoi tao mang
+        prufer = new int[n-1]; // khoi tao mang
+        degree = new int[n+1]; // khoi tao mang
         for (int i = 0; i < degree.length ; i++) {
-            degree[i] = 0;
+            degree[i] = 0; // gan bac cho tat ca cac dinh bang 0
         }
         System.out.println("Nhập các cạnh của cây :");
         for(int i = 0; i < tree.length; i++){
+			System.out.print("Nhap vao canh "+i+": ");
             tree[i][0] = scan.nextInt();
             tree[i][1] = scan.nextInt();
         }
-        for (int i = 0; i < tree.length; i++) {
+        for (int i = 0; i < tree.length; i++) { // tinh bac cua cac dinh
             int[] tmp = tree[i];
             degree[tmp[0]] +=1;
             degree[tmp[1]] +=1;
@@ -31,23 +32,23 @@ public class Tree {
 
         }
         for (int i = 0; i < prufer.length ; i++) {
-            int v = 1;
+            int v = 1; // bat dau tu dinh so 1
             while (degree[v] != 1){
-                v++;
+                v++; // tim dinh co bac bang 1
             }
-            for (int j = 0; j <  tree.length; j++) {
-                if(v == tree[j][0] && degree[tree[j][1]] != 0){
-                    degree[tree[j][0]] -= 1;
+            for (int j = 0; j <  tree.length; j++) { // duyet qua tat ca cac canh trong cay
+                if(v == tree[j][0] && degree[tree[j][1]] != 0){ // tim canh chua dinh bac bang 1
+                    degree[tree[j][0]] -= 1; // giam bac cua nhung dinh nam trong canh bi xoa
                     degree[tree[j][1]] -= 1;
-                    prufer[i] =  tree[j][1];
-                }else if(v == tree[j][1] && degree[tree[j][0]] != 0){
-                    degree[tree[j][0]] -= 1;
+                    prufer[i] =  tree[j][1]; // gan dinh bac 1 vao prufercode
+                }else if(v == tree[j][1] && degree[tree[j][0]] != 0){ // tim canh chua dinh bac bang 1
+                    degree[tree[j][0]] -= 1; // giam bac cua nhung dinh nam trong canh bi xoa
                     degree[tree[j][1]] -= 1;
-                    prufer[i] =  tree[j][0];
+                    prufer[i] =  tree[j][0]; // gan dinh bac 1 vao prufercode
                 }
             }
         }
-        System.out.println("Prufer code của cây là: ");
+        System.out.println("Prufer code của cây là: "); // thuc hien in ra prufercode
         for (int i = 0; i < n-1 ; i++) {
             System.out.print(prufer[i]+" ");
         }
